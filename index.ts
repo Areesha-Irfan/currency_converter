055a830414fc2e464565bdbe74710d6e8f2d8176
Areesha-Irfan <163674228+Areesha-Irfan@users.noreply.github.com>
2024-06-06 09:23:55 -0700

    My currency converter

---
+++ index.ts
@@ -0,0 +1,51 @@
+#! usr/bin/env code 
+import inquirer from "inquirer";
+
+const currency : any = {
+    USD :   1,
+    PKR :  280,
+    EUR :  0.91,
+    INR : 74.57,
+    GBD : 0.76
+};
+
+let ans = await inquirer.prompt(
+ [
+ 
+   {
+        name : "from",
+        message : "Enter from currency",
+        type : "list",
+        choices : ["USD", "PKR", "EUR", "INR", "GBD"]
+
+    },
+    {
+
+        name : "to",
+        message : "Enter to currency",
+        type : "list",
+        choices : ["USD", "PKR", "EUR", "INR", "GBD"]
+       
+    },
+    {
+        name : "amount",
+        type : "number",
+        message : "Enter your amount"
+
+    }
+ 
+ ]
+
+);
+
+let fromAmount = currency[ans.from];
+let toAmount = currency[ans.to]; 
+let useramount = ans.amount;
+let baseAmount = useramount / fromAmount;  //USD base currency
+let convertedAmount = baseAmount * toAmount;
+let amountWithoutDecimal = Math.floor(convertedAmount);
+console.log(amountWithoutDecimal);
+
+
+
+
